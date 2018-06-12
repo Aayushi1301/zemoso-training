166b146bd1fbcbd926026960a667a416a96b2e37
Aayushi <aayushi@zemosolabs.com>
2018-06-12 12:37:29 +0530

    Setup of JAVA Assignment 1 in IntelliJ

---
+++ JAVA1/src/test/SampleTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void testSample(){
+        assertTrue("Testing for true", false);
+    }
+
+}
